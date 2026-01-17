1c3f296326d1a6d94098a45194b6b9340f0d4bdc
Earl Cameron <mr.e.cameron@gmail.com>
2026-01-17 02:44:21 -0500

    Build to docs folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Iten/',
+  build: {
+    outDir: 'docs'
+  },
   server: {
     port: 5173,
     host: '127.0.0.1',
